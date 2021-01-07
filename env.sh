64042a6ec38cd82b14bc427714fe6430452537c6
Kazuki Sawada <kazuki@6715.jp>
2021-01-07 22:09:43 +0900

    add deploy script

--- env.sh
+++ env.sh
@@ -1,12 +1,7 @@
-ENGINE="nextjs-blog-scaffold"
+BLOG_HOST="6715.jp"
 
 export ARTICLES_DIR="../articles"
 export EXTERNAL_DIR="../articles/external"
-
-export NEXT_PUBLIC_LANG="ja"
-export NEXT_PUBLIC_BLOG_TITLE="6715.jp"
-export NEXT_PUBLIC_BLOG_DESCRIPTION="sekaiのブログ"
-export NEXT_PUBLIC_BLOG_URL="https://6715.jp"
 
 export NEXT_PUBLIC_AUTHOR_NAME="sekai"
 export NEXT_PUBLIC_AUTHOR_IMAGE="https://res.cloudinary.com/narusejun/image/twitter_name/h_200/sekai67.jpg"
@@ -15,5 +10,10 @@ export NEXT_PUBLIC_AUTHOR_TWITTER_URL="https://twitter.com/sekai67"
 export NEXT_PUBLIC_AUTHOR_FACEBOOK_URL="https://www.facebook.com/sawada.kazuki"
 export NEXT_PUBLIC_AUTHOR_BIO="インフラとDXが好き"
 export NEXT_PUBLIC_COPYRIGHT="2016 $NEXT_PUBLIC_AUTHOR_NAME"
+
+export NEXT_PUBLIC_LANG="ja"
+export NEXT_PUBLIC_BLOG_TITLE="$BLOG_HOST"
+export NEXT_PUBLIC_BLOG_DESCRIPTION="$NEXT_PUBLIC_AUTHOR_NAMEのブログです"
+export NEXT_PUBLIC_BLOG_URL="https://$BLOG_HOST"
 
 export NEXT_PUBLIC_GA_MEASUREMENT_ID="G-XKSQ648BQ2"
--- tasks.sh
+++ tasks.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
+OUTPUT_DIR="out"
+ENGINE_DIR="nextjs-blog-scaffold"
+
 _npm() {
-	npm --prefix=$ENGINE $@
+	npm --prefix=$ENGINE_DIR "$@"
+}
+_git() {
+	git -C $OUTPUT_DIR "$@"
 }
 
 _link_dir() {
 	mkdir -p $1
-	rm -rf "$ENGINE/$1"
-	ln -s "../$1" "$ENGINE/$1"
+	rm -rf "$ENGINE_DIR/$1"
+	ln -s "../$1" "$ENGINE_DIR/$1"
 }
 
 _init() {
+	_link_dir $OUTPUT_DIR
 	_link_dir public
-	_link_dir out
 	_npm install
 }
 
@@ -25,6 +31,16 @@ build() {
 	_init
 	_npm run build
 	_npm run export
+}
+
+deploy() {
+	printf $BLOG_HOST > $OUTPUT_DIR/CNAME
+	_git init
+	_git add -A
+	_git config user.email "$GITHUB_ACTOR@users.noreply.github.com"
+	_git config user.name "$GITHUB_ACTOR"
+	_git commit -m "built from $GIT_COMMIT_SHA"
+	_git push -f "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git" HEAD:gh-pages
 }
 
 source env.sh
